689d80b7e6fb5a4e0d6fcd8b5bc3b97d2d9e03e3
melonchelo <56135571+melonchelo@users.noreply.github.com>
2022-06-16 16:11:26 -0300

    start

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.use("/views", express.static(__dirname + "/views"));
+app.set("view engine","ejs")
+
+// Example protected and unprotected routes
+app.get('/', (req, res) => res.render('index'))
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+// Auth Routes
+app.get('/logout', (req, res) => {
+    res.redirect('/');
+})
+
+app.listen(4000, () => console.log(`Example app listening on port ${4000}!`))
