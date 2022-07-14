477d5cfe37208a19756f2e411ba844e9869084f3
your name <yout@example.com>
2022-07-14 14:25:05 -0300

    updates

--- app.js
+++ app.js
@@ -5,11 +5,5 @@ app.set("view engine","ejs")
 
 // Example protected and unprotected routes
 app.get('/', (req, res) => res.render('index'))
-app.get('/failed', (req, res) => res.send('You Failed to log in!'))
-
-// Auth Routes
-app.get('/logout', (req, res) => {
-    res.redirect('/');
-})
-/*hola guille*/
-app.listen(4000, () => console.log(`Example app listening on port ${4000}!`))
+/*server host*/
+app.listen(4000, () => console.log(`inicie chrome en localhost:${4000}!`))
